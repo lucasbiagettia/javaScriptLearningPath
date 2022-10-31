68a4ae76ea4da6bba3991d6b2e57e08287a57c08
lucasbiagetti <lucasbiagetti@gmail.com>
2022-10-31 00:09:59 -0300

    04

---
+++ 04ConversionTipos.js
@@ -0,0 +1,41 @@
+// tipos de datos
+// alfanumerico
+const nombrePasajero = "pedro";
+const nombrePasajero = "ramon"
+// no se puede porque es constante
+const valorBoleto = 1650;
+let valor = "valor";
+console.log(valorBoleto)
+
+// logicos
+
+let boolean = true;
+// tipos
+// Undefinded
+// Boolean
+// Number
+// String
+// BigInt
+// Symbol
+ /// - no primitivos
+ // Null
+ // Object
+
+ // Operaciones aritmeticas
+const tasaEmbarque = 60 / 100;
+ let suma = (1700 + 54 + 54 )* tasaEmbarque;
+
+ // orden de precedencia de operaciones
+ 
+ // concatenación de texto
+ let nombre = "nombre " + "nono";
+
+ // si uso parseInt o parseFloat los convierte a int o float
+ let parsed = parseInt('1000');
+ // parsed va a valer 1000
+
+ let nAN = parseInt("un no numero"); // retorna not a number
+
+
+
+
