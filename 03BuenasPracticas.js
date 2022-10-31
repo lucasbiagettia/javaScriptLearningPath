50f4ef6721a947d0ab35bcba1fb3f228d1747de7
lucasbiagetti <lucasbiagetti@gmail.com>
2022-10-30 23:57:44 -0300

    03

---
+++ 03BuenasPracticas.js
@@ -0,0 +1,12 @@
+let nombreEmpleado = "José Pérez";
+let NombreEmpleado = "Pedro Ramírez";
+// es case Sensitive
+
+let edadEmpleado = 40;
+// es libre o debilmente tipado
+edadEmpleado = "cuarenta";
+// acepta este cambio
+
+console.log(edadEmpleados);
+// sale error, lo muestra en pantalla como referenceError
+
