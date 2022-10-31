f5e5f0c73a32b3a449e6eb505b982fc6f55f31a3
lucasbiagetti <lucasbiagetti@gmail.com>
2022-10-30 23:50:38 -0300

    01  y 02

---
+++ 02NombreVariables.js
@@ -0,0 +1,7 @@
+// no usar numeros, acentos ni ñ
+// empezar con minuscula
+var nombreEmpleado = "Milton";
+// usar var por convención
+var direccion = "Avenida 105";
+var costo = 150;
+let tasaEmbarque = 50.50;
