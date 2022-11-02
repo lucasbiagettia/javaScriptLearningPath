831dcb0123e9bfbc17ae5c1773ad863e4efda71e
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-02 08:28:48 -0300

    06

---
+++ 06Templates.js
@@ -0,0 +1,11 @@
+const nombrePasajero = "leandro";
+const apellidoPasajero = "fuentes";
+
+let nombreCompleto = `el nombre completo es ${nombrePasajero} ${apellidoPasajero}`
+console.log(nombreCompleto);
+console.log ("temp");
+
+// -
+
+let segundoPasajaro = 'diego castillo';
+console.log(`el nombre del segundo pasajero es: ${segundoPasajaro}`);
