1f173bf915bf7df58658bcf73235def674b503b7
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2022-11-04 10:05:42 -0300

    08

---
+++ 07Listas.js
@@ -0,0 +1,16 @@
+const ciudad1 = "Bogotá";
+const ciudad2 = "Lima";
+
+
+const ciudadesDisponibles = new Array("Bogotá", "Lima");
+// definicion de una lista con tipo de dato alfanumerico
+console.log (ciudadesDisponibles);
+
+//Definicion deuna lista con forma abreviada
+//Los arreglos no tienen tamaño definido
+const paisesDisponibles = ["Colombia", "peru"];
+
+{
+    paisesDisponibles.push("uruguay");
+    
+}
