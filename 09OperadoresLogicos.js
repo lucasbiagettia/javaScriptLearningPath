0c322611ec0b5f6f4dbddcbfd600a97debb0be0c
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-06 22:00:56 -0300

    09

--- 09OperadoresLogicos.js
+++ 09OperadoresLogicos.js
@@ -7,5 +7,15 @@ if (ciudadesDisponibles.indexOf(ciudadDestino) >= 0){
 }else{
     console.log("No está");
 }
+// == no verifica tipo de dato
+const valorPasaje  = "1000";
+if (valorPasaje == 1000){
+    console.log( "vale mil");
+}
 
-
+// === verifica tipo de dato
+if (valorPasaje === 1000){
+    console.log( "vale mil");
+} else {
+    console.log ("distinto dato");
+}
