7d6fa30ad3fa8b50e1b5eb972b427c141288f374
lucasbiagetti <lucasbiagetti@gmail.com>
2022-11-06 21:56:59 -0300

    09

---
+++ 09OperadoresLogicos.js
@@ -0,0 +1,11 @@
+// Operadores de comparación
+const ciudadDestino = "Bogotá";
+const ciudadesDisponibles = new Array ("Bogotá", "Rosario");
+
+if (ciudadesDisponibles.indexOf(ciudadDestino) >= 0){
+    console.log("Está");
+}else{
+    console.log("No está");
+}
+
+
